d873c050c962e9eb134bdf503b361258697e4cc3
Charlie <crck@outlook.com>
2015-07-28 18:04:18 +0100

    Adds seed data and men's view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :products
+  root "products#index"
+  resources :products do
+    member do
+      get "male"
+      get "female"
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/features/products_features_spec.rb
+++ spec/features/products_features_spec.rb
@@ -1,21 +1,35 @@
 require 'rails_helper'
 
 feature 'homepage' do 
-  context 'A user can choose what gender they want to buy for and see a list of related items' do
-    scenario 'should display a list of mens clothing items' do 
-      visit '/products'
-      expect(page).to have_content 'Male'
-      expect(page).to have_content 'Female'
-    end
+  scenario 'A user can choose what gender they want to buy for' do 
+    visit '/products'
+    expect(page).to have_content 'Male'
+    expect(page).to have_content 'Female'
+  end 
+
+  scenario 'should display a navbar' do 
+    visit '/products'
+    expect(page).to have_content 'Home'
+    expect(page).to have_content 'Men\'s'
+    expect(page).to have_content 'Women\'s'
+    expect(page).to have_content 'Gift Vouchers'
   end
 
-    scenario 'should display a navbar' do 
+  context 'when clicking on gender to shop for' do
+    scenario 'page should display list of relevant clothing items' do 
       visit '/products'
       expect(page).to have_content 'Home'
       expect(page).to have_content 'Men\'s'
       expect(page).to have_content 'Women\'s'
       expect(page).to have_content 'Gift Vouchers'
     end
+
+    scenario 'should display men\'s clothing'do
+      visit '/products'
+      page.find('#mens').click
+      expect(current_path).to have_content "/products/1/male"
+    end
+  end
 end
 
       # scenario 'should display a list of mens clothing items' do 
