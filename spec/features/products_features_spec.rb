31f6cfd58a15b542055f2acf6629a80c6a68532b
Charlie <crck@outlook.com>
2015-07-28 13:24:09 +0100

    Adds navbar

--- spec/features/products_features_spec.rb
+++ spec/features/products_features_spec.rb
@@ -8,6 +8,14 @@ feature 'homepage' do
       expect(page).to have_content 'Female'
     end
   end
+
+    scenario 'should display a navbar' do 
+      visit '/products'
+      expect(page).to have_content 'Home'
+      expect(page).to have_content 'Men\'s'
+      expect(page).to have_content 'Women\'s'
+      expect(page).to have_content 'Gift Vouchers'
+    end
 end
 
       # scenario 'should display a list of mens clothing items' do 
